826662d1d3279884badb24e97b49242558df804a
JeyK <s.merlinjeyakumar@gmail.com>
2025-07-22 22:03:19 +0400

    chore: Update targetSdkVersion to 35
    This commit updates the `targetSdkVersion` in `Configs.kt` from 33 to 35.
    This change is necessary to align the target SDK with the latest
    Android API level and to leverage new features and improvements. There
    are no breaking changes introduced by this update.

--- buildSrc/src/main/java/Configs.kt
+++ buildSrc/src/main/java/Configs.kt
@@ -3,7 +3,7 @@ object Configs {
     const val kotlinVersion = "1.9.0"
     const val classpathGradleVersion = "7.5"
     const val kotlinGradlePlugin = "1.8.20"
-    const val targetSdkVersion = 33
+    const val targetSdkVersion = 35
     const val minSdkVersion = 21
     const val compileSdkVersion = 35
     const val versionCode = 1
