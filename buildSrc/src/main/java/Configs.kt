4c2cae36eda02ff8ced61f6ad8c6ce77ed69c202
JeyK <s.merlinjeyakumar@gmail.com>
2026-03-04 21:13:52 +0530

    feat: Add Configs object to centralize application build constants.

--- buildSrc/src/main/java/Configs.kt
+++ buildSrc/src/main/java/Configs.kt
@@ -6,6 +6,6 @@ object Configs {
     const val targetSdkVersion = 35
     const val minSdkVersion = 21
     const val compileSdkVersion = 35
-    const val versionCode = 1
-    const val versionName = "1.0.2"
+    const val versionCode = 2
+    const val versionName = "1.0.3"
 }
